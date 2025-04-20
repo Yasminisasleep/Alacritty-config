55a8646c14abccf04ed38cd998adae069dac8e13
Yasmine Benali <yasmine.benali@epita.fr>
2025-04-21 01:26:16 +0200

    added dotfils : zshrc, starship.toml, alacritty.toml and symlimks in the install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+ln -sf ~/dotfiles/zshrc ~/.zshrc
+ln -sf ~/dotfiles/config/starship.toml ~/.config/starship.toml
+ln -sf ~/dotfiles/config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
